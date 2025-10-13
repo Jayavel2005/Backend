ff5f133f29c691397bc422891731289c79e29d94
Jayavel <jayavel1520@gmail.com>
2025-10-13 21:56:45 +0530

    File writing

--- NodeJs/FileSystem/app.js
+++ NodeJs/FileSystem/app.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
+const http = require('http');
 
 // async file reading which uses callback
 
 // fs.readFile("E:/NodeJS/Backend/NodeJs/FileSystem/sample.txt", "utf8", (err, data) => {
 //     if (err) {
 //         console.error("Failed to read file:", err.message);
-//         return; 
+//         return;
 //     }
 //     console.log(data);
 // });
@@ -17,15 +18,16 @@ const fs = require("fs");
 
 // async file writing
 
-fs.writeFile("E:/NodeJS/Backend/NodeJs/FileSystem/output.txt", 'Hello, I am from file writing', (err) => {
-    if (err) {
-        console.log(err.message);
-        return
-    }
-    console.log("File written succesfully!");
+// fs.writeFile("E:/NodeJS/Backend/NodeJs/FileSystem/output.txt", 'Hello, I am from file writing', (err) => {
+//     if (err) {
+//         console.log(err.message);
+//         return
+//     }
+//     console.log("File written succesfully!");
 
-})
+// })
 
-// sync file writing
+// // sync file writing
 
-fs.writeFileSync("E:/NodeJS/Backend/NodeJs/FileSystem/output.txt", "Vanakam da Mapla!");
+// fs.writeFileSync("E:/NodeJS/Backend/NodeJs/FileSystem/output.txt", "Vanakam da Mapla!");
+
