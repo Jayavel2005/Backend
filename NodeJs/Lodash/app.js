648aae5c4d411e39c670e1125d55b2b50ea812cc
Jayavel <jayavel1520@gmail.com>
2025-10-13 22:06:11 +0530

    Working on Third Party package

---
+++ NodeJs/Lodash/app.js
@@ -0,0 +1,6 @@
+const lodash = require('lodash');
+
+console.log(lodash.add(1, 2));
+console.log(lodash.partition([1, 2, 3, 4, 5], (n) => n % 2 == 0));
+console.log(lodash.capitalize("hello world"));
+
---
+++ NodeJs/Lodash/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const lodash = require('lodash');
+
+console.log(lodash.add(1, 2));
+console.log(lodash.partition([1, 2, 3, 4, 5], (n) => n % 2 == 0));
+
