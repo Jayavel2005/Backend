1d040f409614085542bca948b3d74feb090f1274
Jayavel <jayavel1520@gmail.com>
2025-10-18 22:05:34 +0530

    imp: Request and Response Handling in Node Js

--- NodeJs/server/server.js
+++ NodeJs/server/server.js
@@ -1,12 +1,77 @@
-const http = require('http');
+// const http = require('http');
+
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', "text/plain")
+//     res.end("Hello World!");
+// })
+
+// server.listen(3000, "127.0.0.1", () => {
+//     console.log("Listening on port 3000");
+// });
+
+const http = require("http");
 
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', "text/plain")
-    res.end("Hello World!");
-})
+    // if (req.method === "GET" && req.url === "/") {
+    //     const statusCode = 200;
+    //     res.writeHead(statusCode, { "content-type": "text/plain" });
+    //     res.write("Hello World!");
+    //     res.end();
+    // } else {
+    //     const statusCode = 404;
+    //     res.writeHead(statusCode, { "content-type": "text/plain" });
+    //     res.write("404 Not Found!");
+    //     res.end();
+    // }
 
-server.listen(3000, "127.0.0.1", () => {
-    console.log("Listening on port 3000");
+    // if (req.method === "GET" && req.url === "/") {
+    //     const statusCode = 200;
+    //     const form = `
+    //     <form method="POST" action="/submit">
+    //         <input type="text" name="username" placeholder="Enter your username">
+    //         <input type="password" name="password" placeholder="Enter your password">
+    //         <button type="submit">Submit</button>
+    //     </form>
+    //     `
+    //     res.writeHead(statusCode, { "content-type": "text/html" });
+    //     res.write(form);
+    //     res.end();
+    // }
+
+
+    // if (req.method === "POST" && req.url === "/submit") {
+    //     let body = ""
+    //     req.on("data", (chunk) => {
+    //         body += chunk.toString();
+    //     })
+    //     req.on("end", () => {
+    //         const statusCode = 200
+    //         res.writeHead(statusCode, { 'content-type': "application/json" });
+    //         res.write(JSON.stringify({ "message": "Data received", "data": body }));
+    //         res.end();
+    //     })
+    // }
+
+    // handling query parametres
+    if (req.method === "GET" && req.url.startsWith('/search')) {
+        const queryParams = req.url.split('?')[1];
+        console.log(queryParams);
+        const username = new URLSearchParams(queryParams).get("username");
+        const password = new URLSearchParams(queryParams).get("password");
+        res.writeHead(200, { "content-type": "application/json" });
+        res.write(JSON.stringify({ username, password }));
+        res.end();
+
+
+    }
 });
+
+const PORT = 3000;
+
+server.listen(PORT, "127.0.0.1", () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+})
