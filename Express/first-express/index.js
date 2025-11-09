eff5be08930434cac6a6696293a58e94b0cde1e5
Jayavel <jayavel1520@gmail.com>
2025-11-09 21:36:13 +0530

    feat: add search endpoint with keyword and greet parameters

--- Express/first-express/index.js
+++ Express/first-express/index.js
@@ -11,6 +11,13 @@ app.get("/user/:userName", (req, res) => {
     res.send(`Hello ${req.params.userName}`);
 })
 
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword;
+    const greet = req.query.greet;    
+    res.send(`Searching for ${keyword} with greeting ${greet}`);
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 
