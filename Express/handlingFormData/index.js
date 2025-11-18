4ec8e56cbf75263630788179b55cd72c6a4f30e3
Jayavel <jayavel1520@gmail.com>
2025-11-18 21:32:18 +0530

    feat: integrate mongoose for MongoDB connection

--- Express/handlingFormData/index.js
+++ Express/handlingFormData/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import route from "./routers.js";
+import mongoose from "mongoose";
 
 
 const app = express()
 const PORT = 3000;
+const MONGODB_URL = `mongodb+srv://JAYAVEL:jaya2005vel@cluster0.pghxx.mongodb.net/express`;
+
+await mongoose.connect(MONGODB_URL).then(()=>{
+    console.log("MongoDB connected successfully");
+})
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(route);
